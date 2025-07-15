0fd22d0a91ab1c04d26b5174bcad5c24ff2ae3a9
Ravindra7330 <ravindarravi87@gmail.com>
2025-07-15 11:15:44 +0530

    added disable prop

--- src/App.jsx
+++ src/App.jsx
@@ -250,7 +250,7 @@ function App() {
       "pause"
       )}
           </button>
-           <button type="button" title={!isPaused ? "Already Unpaused" : "click to unpause the contract"}  onClick={unpauseContract} className="text-white bg-blue-700  hover:bg-blue-800 focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ml-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+           <button type="button" title={!isPaused ? "Already Unpaused" : "click to unpause the contract"} disabled={!isPaused} onClick={unpauseContract} className="text-white bg-blue-700  hover:bg-blue-800 focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ml-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
              {isUnpauseLoading ? (
         <>
         <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3  text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
